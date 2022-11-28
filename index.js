bdd1d422494516b15ddaee7dc7a4553730f560b3
mdiibrahim <mdiibrahim549@gmail.com>
2022-11-28 21:04:35 +0600

    dashboard api is done

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
-require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(express.json());
@@ -59,9 +59,63 @@ async function run() {
             }
             const token = jwt.sign({ email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             return res.send({ accessToken: token });
-            
+
         });
         app.post('/users', async (req, res) => {
+            const user = req.body;
+            const copiedUser = await usersCollection.findOne(user);
+            if (!copiedUser) {
+
+                res.send(await usersCollection.insertOne(user));
+            }
+            res.status(200).send({ acknowledged: 'successfull' })
+        })
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+        // app.get('/users/:email', async (req, res) => {
+        //     const email = req.params.email;
+        //     console.log(email)
+        //     const query = { email }
+        //     const user = await usersCollection.findOne(query);
+        //     console.log(user)
+        //     if (user?.role === 'admin') {
+        //         res.send({ isAdmin: true })
+        //     }
+        //     if (user?.role === 'seller') {
+        //         res.send({ isSeller: true })
+        //     }
+        //     if (user?.role === 'buyer') {
+        //         res.send({ isBuyer: true })
+        //     }
+        //     res.status(401).send('unauthorized access');
+
+
+        // })
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log(email)
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        })
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log(email)
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isSeller: user?.role === 'seller' });
+        })
+        app.get('/users/buyer/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log(email)
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isBuyer: user?.role === 'buyer' });
+        })
+        app.get('/users', async (req, res) => {
             const user = req.body;
             res.send(await usersCollection.insertOne(user));
         })
@@ -72,7 +126,7 @@ async function run() {
     }
 }
 run().catch(console.log);
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.send('Tukitaki-টুকিটাকি server is running......');
 })
 
