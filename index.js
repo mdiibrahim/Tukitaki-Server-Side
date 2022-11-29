35abd12647d049f2997c695d09b9bb0133ea56b6
mdiibrahim <mdiibrahim549@gmail.com>
2022-11-30 03:19:36 +0600

    products deleted api done

--- index.js
+++ index.js
@@ -195,6 +195,13 @@ async function run() {
             })
             res.send(filter)
         })
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const product = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(product);
+            res.send(result);
+
+        })
 
 
     } finally {
